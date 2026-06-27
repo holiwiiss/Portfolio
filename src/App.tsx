63579196015450caae1ee30e5b2efb7a76ad050a
Paula Ramiro Olivas <paularamiro11@gmail.com>
2026-06-27 14:02:41 +0200

    feature: base proyect

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import MainLeftbar from './components/main-leftbar/main-leftbar'
+
+function App() {
+
+  return (
+    <>
+      <MainLeftbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/main-leftbar/main-leftbar.tsx
@@ -0,0 +1,13 @@
+import "./main-leftbar.scss"
+
+const MainLeftbar = () => {
+  return(
+    <>
+      <nav className="main-leftbar">
+        
+      </nav>
+    </>
+  )
+}
+
+export default MainLeftbar
